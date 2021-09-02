557404c524030a2971eacc75e06c48160ad0df73
yasodayks <yasoda.yks@gmail.com>
2021-09-02 15:40:39 +0530

    first commit

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import { chartreuse } from 'color-name';
+import React from 'react';
+import './Chart.css';
+import ChartBar from'./ChartBar';
+
+const Chart = props => {
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const dataPointMax= Math.max(...dataPointValues);
+
+  return (<div className='chart'>
+      {props.dataPoints.map((dataPoint) => (
+          <ChartBar 
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={dataPointMax}
+          label={dataPoint.label}/>
+      ))}
+  </div>);
+};
+export default Chart;   
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,31 @@
+import './NewExpense.css';
+import ExpenseForm from './ExpenseForm';
+import React, {useState} from 'react';
+
+const NewExpense = (props) => {
+  let [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+     setIsEditing(true);
+  }
+
+  const stopEditingHandler = () => {
+    setIsEditing(false); 
+ }
+ const saveExpenseDataHandler = (enteredExpenseData) =>{
+    const expenseData = {
+        ...enteredExpenseData,
+        id: Math.random.toString()
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false); 
+ }
+  return (
+    <div className="new-expense">
+      { !isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+      { isEditing  && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler}/> }
+    </div>
+  );
+};
+
+export default NewExpense;
